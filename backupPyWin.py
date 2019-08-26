0164e89aa415266f3d1202f3d0276554c0ef2ce1
jadsonmarliere <jds.mdo@gmail.com>
2019-08-26 17:08:46 -0300

    main Structure

---
+++ backupPyWin.py
@@ -0,0 +1,11 @@
+# Script para copiar arquivos, comprimir e armazenar em local definido
+
+import os
+import zipfile
+import shutil
+
+def processo():
+
+def fazCopia():
+
+if __name__ == '__main__':
