f08b789cab875c7ded2f1c71575602d71eabc972
jadsonmarliere <jds.mdo@gmail.com>
2019-08-30 11:53:59 -0300

    Update backupPyWin3x.py

--- backupPyWin3x.py
+++ backupPyWin3x.py
@@ -18,3 +18,15 @@ os.chdir(fonte)
 for arquivo in arquivos:
 	with open(arquivo) as arq:
 		print(arquivo,arq.read())
+
+for arquivo in arquivos:
+	if os.path.isfile(arquivo):
+		shutil.copy(arquivo, destino)
+	
+		'''
+		#Codigo para mover os arquivos e sobrescrever
+		print(destino)
+		destino_total = os.path.join(destino, arquivo)
+		print(destino_total)
+		shutil.move(file, destino_total)
+		'''
